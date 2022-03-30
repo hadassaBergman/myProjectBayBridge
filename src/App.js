135d25ba4fd5b92cad04a57e074afe256cab8069
hadassaBergman <hadassawatt@gmail.com>
2022-03-30 19:30:40 +0300

    my project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useReducer, useState} from 'react';
 import './App.css';
-import Dummy_Component from "dummy-component";
+import data from "./data.json";
+const fs = require('fs');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Dummy_Component/>
-      </header>
-    </div>
-  );
+
+    const [products, setProducts] = useState(data);
+    console.log("products",products);
+
+    const [sum, setSum] = useState(0);
+
+    useEffect(() => {
+        storeData(products, './data.json')
+    }, []);
+
+    useEffect(() => {
+        let sumOfProducts=0;
+        for(let item of products){
+            sumOfProducts = sumOfProducts+((item.price)*(item.quantity));
+        }
+        setSum(sumOfProducts);
+    }, [products]);
+
+    const storeData = (data, path) => {
+        try {
+            fs.writeFileSync(path, JSON.stringify(data))
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    return (<>
+            <div>Prix d'expedition:15 euros</div>
+            <div>Prix total:{sum} euros</div>
+        <div className="App">
+            {products.map((item) => (<div className={'row'}>
+                <div className={"item"}> <span className={"item_title"}>Produit:</span> {item.product} </div>
+                <div className={"item"}> <span className={"item_title"}>Prix:</span>{item.price} euros </div>
+                <div className={"item"}> <span className={"item_title"}>Quantite:</span>{item.quantity} </div>
+                <button onClick={()=>{
+                    setProducts((ps) => ps.map((product) => (
+                        {
+                        ...product,
+                        quantity: product.quantity+1,
+                    })));
+                    storeData(products, './data.json')
+                }}> + </button>
+                <button onClick={()=>{
+                    setProducts((ps) => ps.map((product) => (
+                        {
+                        ...product,
+                        quantity: product.quantity-1,
+                    })));
+                    storeData(products, './data.json')
+                }}>-</button>
+            </div>))}
+        </div></>
+    );
 }
 
 export default App;
